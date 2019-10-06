abe900fb4d6ece28e670dddf5b90a13828293793
Henry Ikoh <think.senpai@gmail.com>
2019-10-06 05:35:53 +0100

    email verification done

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ var rp = require("request-promise");
 var request = require('request');
 admin.initializeApp();
 const Nexmo = require('nexmo');
+const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
+
+
+// Initialinng a admin on local server, **Would revert to this for only local testing**
+
+// var serviceAccount = require("/Users/mac/Downloads/shecluded-firebase-adminsdk-c8lsf-7702979dd3.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://shecluded.firebaseio.com"
+// });
+//Admin local server ends here****
 
 //Data base
 var db = admin.firestore()
@@ -20,6 +32,8 @@ const nexmo = new Nexmo({
   apiKey: process.env.NEXMO_APIKEY,
   apiSecret: process.env.NEXMO_APISECRET,
 });
+
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -68,8 +82,42 @@ exports.PhoneAuthVerify = functions.https.onCall((load) => {
               }
           });
 
-    }))
+    }));
     
 
 
+});
+
+exports.SendWelcomeMail = functions.https.onCall( async(email,context) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  //Actioncode is for the firebase email verification. It passes in varguements that change the behaviour of the link
+  const actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for
+    // this URL must be whitelisted in the Firebase Console.
+    url: 'https://shecluded.web.app/',
+    
+  };
+
+ 
+ return admin.auth().generateEmailVerificationLink(context.auth.token.email, actionCodeSettings)
+  .then(async(link) => {
+    const msg = {
+      to: context.auth.token.email,
+      from: 'test@example.com',
+      subject: 'Sending with Twilio SendGrid is Fun',
+      templateId: 'd-b626671df4f34d34b5316000ec89720c',
+      dynamic_template_data: {
+       subject: 'Testing Templates',
+       firstname: context.auth.token.name,
+      //  name: context.auth.token.name,
+       link: link
+      }
+    };
+    
+    await sgMail.send(msg)
+    return {success:true}
+  })
+  .catch((error) => {
+    return{error}
+  });
 });
